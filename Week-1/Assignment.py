8414ed990545b52e85be08360c881c4bea784677
sri_lavanya_tamatapu <138578739+Lavanya8484@users.noreply.github.com>
2025-06-08 09:33:16 +0530

    Update Assignment.py

--- Week-1/Assignment.py
+++ Week-1/Assignment.py
@@ -1,1 +1,16 @@
+# Lower-Triangle
+rows = 0
+for i in range(rows,0,-1):
+  print("  " * (rows - i) + "* " * i)
 
+# Upper-Triangle
+rows = 5
+for i in range(1, rows + 1):
+    print("* " * i)
+  
+# Pyramid
+rows = 5
+for i in range(1, rows + 1):
+    spaces = ' ' * (rows - i)
+    stars = '*' * (2 * i - 1)
+    print(spaces + stars)
